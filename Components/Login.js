186773606b778c9ba0da51775695355fd1b0ead3
Marcos Duarte <pe.marcos30@gmail.com>
2023-09-26 20:35:34 -0300

    Estilizando Login

--- Components/Login.js
+++ Components/Login.js
@@ -1,24 +1,25 @@
-import { StyleSheet, Text, View } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import { Input, Button } from 'react-native-elements';
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
-import { useState } from 'react';
+import { StyleSheet, Text, View } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { Input, Button } from "react-native-elements";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
+import NavBar from "./NavBar";
 
 const auth = getAuth();
 
-export default function Login({ updateUserLoggedIn, navigation  }) {
+export default function Login({ updateUserLoggedIn, navigation }) {
   const [value, setValue] = useState({
-    email: '',
-    password: '',
-    error: ''
-  })
+    email: "",
+    password: "",
+    error: "",
+  });
 
   async function signIn() {
-    if (value.email === '' || value.password === '') {
+    if (value.email === "" || value.password === "") {
       setValue({
         ...value,
-        error: 'Email and password are mandatory.'
-      })
+        error: "Email and password are mandatory.",
+      });
       return;
     }
 
@@ -29,78 +30,83 @@ export default function Login({ updateUserLoggedIn, navigation  }) {
       if (navigation.canGoBack()) {
         navigation.goBack();
       } else {
-        navigation.navigate('Cardapio');
+        navigation.navigate("Cardapio");
       }
-      
-      
     } catch (error) {
       setValue({
         ...value,
         error: error.message,
-      })
+      });
     }
   }
 
   return (
-    <View style={styles.container}>
-      <Text>Signin screen!</Text>
+    <View style={styles.mainContainer}>
+      <NavBar navigation={navigation} nome="Login"/>
+      <View style={styles.container}>
+        {!!value.error && (
+          <View style={styles.error}>
+            <Text>{value.error}</Text>
+          </View>
+        )}
 
-      {!!value.error && <View style={styles.error}><Text>{value.error}</Text></View>}
+        <View style={styles.controls}>
+          <Input
+            placeholder="Email"
+            containerStyle={styles.control}
+            value={value.email}
+            onChangeText={(text) => setValue({ ...value, email: text })}
+            leftIcon={<Icon style={{color: 'white'}} name="envelope" size={16} />}
+          />
 
-      <View style={styles.controls}>
-        <Input
-          placeholder='Email'
-          containerStyle={styles.control}
-          value={value.email}
-          onChangeText={(text) => setValue({ ...value, email: text })}
-          leftIcon={<Icon
-            name='envelope'
-            size={16}
-          />}
-        />
+          <Input
+            placeholder="Password"
+            containerStyle={styles.control}
+            value={value.password}
+            onChangeText={(text) => setValue({ ...value, password: text })}
+            secureTextEntry={true}
+            leftIcon={<Icon style={{color: 'white'}} name="key" size={16} />}
+          />
 
-        <Input
-          placeholder='Password'
-          containerStyle={styles.control}
-          value={value.password}
-          onChangeText={(text) => setValue({ ...value, password: text })}
-          secureTextEntry={true}
-          leftIcon={<Icon
-            name='key'
-            size={16}
-          />}
-        />
-
-        <Button title="Sign in" buttonStyle={styles.control} onPress={signIn} />
+          <Button
+            title="Logar"
+            buttonStyle={styles.control}
+            onPress={signIn}
+          />
+        </View>
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    backgroundColor: "#091014",
+  },
   container: {
     flex: 1,
     paddingTop: 20,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#091014",
+    alignItems: "center",
+    justifyContent: "center",
   },
 
   controls: {
     flex: 1,
-    
   },
 
   control: {
     height: 50,
     width: 350,
-    marginTop: 10,
+    marginTop: 15,
+    color: "white",
   },
 
   error: {
     marginTop: 10,
     padding: 10,
-    color: '#fff',
-    backgroundColor: '#D54826FF',
-  }
+    color: "#fff",
+    backgroundColor: "#D54826FF",
+  },
 });
